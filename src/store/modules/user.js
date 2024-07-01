fbe175d9cd7783ade09a4e068620f9354cdf9a32
ChealseaYao <successsssssss1126@outlook.com>
2024-07-01 17:16:09 +0930

    redux

---
+++ src/store/modules/user.js
@@ -0,0 +1,36 @@
+
+import { request } from "@/utils"
+import { createSlice } from "@reduxjs/toolkit"
+
+
+
+const userStore=createSlice({
+    name:"user",
+    initialState:{
+        token: ''
+    },
+    //同步修改方案
+    reducers: {
+        setToken (state,actions){
+            state.token=actions.payload
+        }
+    }
+})
+
+//解构出actionCreater
+const { setToken } = userStore.actions
+
+//获取reducer函数
+const userReducer = userStore.reducer
+
+//异步方法
+const fetchLogin = (loginForm) => {
+    return async (dispatch) => {
+      const res = await request.post('/authorizations', loginForm)
+      dispatch(setToken(res.data.token))
+    }
+  }
+  
+
+export {setToken, fetchLogin}
+export default userReducer
